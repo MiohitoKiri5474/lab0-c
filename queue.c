07b646c782bb5bfbca1e2dadb6a9dd14f55eaa25
MiohitoKiri5474 <lltzpp@gmail.com>
2024-02-24 11:38:19 +0800

    Implement q_new
    Create a list_head and initialize it with INIT_LIST_HEAD.

--- queue.c
+++ queue.c
@@ -14,7 +14,10 @@
 /* Create an empty queue */
 struct list_head *q_new()
 {
-    return NULL;
+    struct list_head *res =
+        (struct list_head *) malloc(sizeof(struct list_head));
+    INIT_LIST_HEAD(res);
+    return res;
 }
 
 /* Free all storage used by queue */
