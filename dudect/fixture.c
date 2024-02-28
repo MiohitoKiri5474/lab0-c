334491b4b674a27c79541a15c71fbc7c5c921c22
MiohitoKiri5474 <lltzpp@gmail.com>
2024-02-28 14:12:43 +0800

    Implement functions in dudect
    To perform the same testing in dudect, I implement the function
    pre_percentiles and added it into doit function.
    
    In pre-percentiles I sorted the array into ascending order, then
    calculate the percentile of each of them.

--- dudect/fixture.c
+++ dudect/fixture.c
@@ -64,9 +64,21 @@ static void differentiate(int64_t *exec_times,
         exec_times[i] = after_ticks[i] - before_ticks[i];
 }
 
+static int cmp(const int64_t *a, const int64_t *b)
+{
+    return (int) (*a - *b);
+}
+
+static int64_t percentile(int64_t *arr, double which, size_t sz)
+{
+    size_t res = (size_t) sz * which;
+    assert(res < sz);
+    return res;
+}
+
 static void update_statistics(const int64_t *exec_times, uint8_t *classes)
 {
-    for (size_t i = 0; i < N_MEASURES; i++) {
+    for (size_t i = 10; i < N_MEASURES; i++) {
         int64_t difference = exec_times[i];
         /* CPU cycle counter overflowed or dropped measurement */
         if (difference <= 0)
@@ -74,6 +86,17 @@ static void update_statistics(const int64_t *exec_times, uint8_t *classes)
 
         /* do a t-test on the execution time */
         t_push(t, difference, classes[i]);
+    }
+}
+
+static void pre_percentiles(int64_t *exec_time)
+{
+    qsort(exec_time, N_MEASURES, sizeof(int64_t),
+          (int (*)(const void *, const void *)) cmp);
+    for (size_t i = 0; i < N_MEASURES; i++) {
+        exec_time[i] = percentile(
+            exec_time, 1 - (pow(0.5, 10) * (double) (i + 1) / N_MEASURES),
+            N_MEASURES);
     }
 }
 
@@ -133,6 +156,7 @@ static bool doit(int mode)
 
     bool ret = measure(before_ticks, after_ticks, input_data, mode);
     differentiate(exec_times, before_ticks, after_ticks);
+    pre_percentiles(exec_times);
     update_statistics(exec_times, classes);
     ret &= report();
 
@@ -170,8 +194,11 @@ static bool test_const(char *text, int mode)
     return result;
 }
 
-#define DUT_FUNC_IMPL(op) \
-    bool is_##op##_const(void) { return test_const(#op, DUT(op)); }
+#define DUT_FUNC_IMPL(op)                \
+    bool is_##op##_const(void)           \
+    {                                    \
+        return test_const(#op, DUT(op)); \
+    }
 
 #define _(x) DUT_FUNC_IMPL(x)
 DUT_FUNCS
